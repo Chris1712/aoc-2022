a853f864980bfb7f553cb9cae2f89ac845cafb92
Chris <christurner5@gmail.com>
2022-12-04 00:35:06 +0000

    Day 1 part 2 solve

--- app/src/main/kotlin/aoc/App.kt
+++ app/src/main/kotlin/aoc/App.kt
@@ -5,6 +5,8 @@ package aoc
 
 fun main() {
     println("AoC 2022:")
-    println("Day 1 solution:")
-    print(mostCals())
+    println("Day 1 part 1 solution:")
+    println(mostCals())  // 70374
+    println("Day 1 part 2 solution:")
+    println(top3Sum())  // 204610
 }
--- app/src/main/kotlin/aoc/Day1.kt
+++ app/src/main/kotlin/aoc/Day1.kt
@@ -6,19 +6,36 @@ import java.lang.Integer.max
  * Return the largest number of calories held by any single elf
  */
 fun mostCals(): Int {
+    return mostCalsTopN(1)[0]
+}
+
+/**
+ * Return the total calories held by the top 3 highest calorie-holding elves
+ */
+fun top3Sum(): Int {
+    return mostCalsTopN(3).sum()
+}
+
+/**
+ * Return ordered list of calories held by top N calorie-holding elves
+ */
+fun mostCalsTopN(n: Int): List<Int> {
     // Get list of food item calorie values. Double line break indicates a different elf
     val lines: List<String> = loadResource("day1-input").split("\n")
 
     var elfHeldCals: Int = 0
-    var maxElfHeldCals: Int = 0
+    var topHeldCals: List<Int> = List(n) {0} // Ordered list to hold n highest values
     for (i in lines.indices) {
         if (lines[i] == "") {
-            maxElfHeldCals = max(elfHeldCals, maxElfHeldCals)
+            // We've finished this elf's items
+            if (elfHeldCals > topHeldCals.minOrNull() ?: 0) {
+                topHeldCals = topHeldCals.subList(1, n).plus(elfHeldCals).sorted()
+            }
             elfHeldCals = 0
             continue;
         } else {
             elfHeldCals += lines[i].toInt()
         }
     }
-    return maxElfHeldCals
+    return topHeldCals
 }
